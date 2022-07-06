415be437992ead65606cf421f2360c4dc8c9530e
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-06 21:37:42 +0700

    Testimonials

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import BootstrapMultiCarouse from "./BootstrapMulti";
+// import BootstrapMultiCarouse from "./BootstrapMulti";
 // import Carouse from "./Carouse";
 // import MultiItemCarouse from "./MultiItemCarousel";
-// import "./styles.css";
+import "./styles.css";
+import Testimonial from "./Testimonial";
 
 export default function App() {
   return (
     <div className="App">
       {/* <Carouse />
-      <MultiItemCarouse /> */}
-      <BootstrapMultiCarouse />
+      <MultiItemCarouse />
+      <BootstrapMultiCarouse /> */}
+      <Testimonial />
     </div>
   );
 }
--- src/BootstrapMulti.js
+++ src/BootstrapMulti.js
@@ -2,19 +2,17 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./BootstrapMulti.css";
-import { multiData } from "./data";
-import {
-  ArrowBackIos,
-  ArrowForwardIos,
-  FavoriteBorder
-} from "@material-ui/icons";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import { data, multiData } from "./data";
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 import { Rating } from "@material-ui/lab";
 
 const PreviousBtn = (props) => {
+  console.log(props);
   const { className, onClick } = props;
   return (
     <div className={className} onClick={onClick}>
-      <ArrowBackIos style={{ color: "white" }} />
+      <ArrowBackIos style={{ color: "white", fontSize: "30px" }} />
     </div>
   );
 };
@@ -22,25 +20,27 @@ const NextBtn = (props) => {
   const { className, onClick } = props;
   return (
     <div className={className} onClick={onClick}>
-      <ArrowForwardIos style={{ color: "white" }} />
+      <ArrowForwardIos style={{ color: "white", fontSize: "30px" }} />
     </div>
   );
 };
 
-const BootstrapMultiCarouse = () => {
+const BootstrapMultiCarousel = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div
+      className="BootstrapMulti"
+      style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+    >
       <div style={{ width: "80%" }}>
         <Slider
           prevArrow={<PreviousBtn />}
           nextArrow={<NextBtn />}
           slidesToShow={4}
           slidesToScroll={3}
+          dots
         >
           {multiData.map((item) => (
-            <div>
-              <Card item={item} />
-            </div>
+            <Card item={item} />
           ))}
         </Slider>
       </div>
@@ -49,7 +49,6 @@ const BootstrapMultiCarouse = () => {
 };
 
 const Card = ({ item }) => {
-  console.log(item);
   return (
     <div
       style={{
@@ -58,58 +57,50 @@ const Card = ({ item }) => {
         padding: "0 10px",
         width: 220,
         boxShadow: "0 1px 6px 0 rgb(32 33 36 / 28%)",
-        borderRadius: "7px"
+        borderRadius: 5,
       }}
     >
       <span style={{ float: "right", padding: "5px" }}>
-        <FavoriteBorder />
+        <FavoriteBorderIcon style={{ color: "gray" }} />
       </span>
       <img
         className="multi__image"
-        alt=""
         src={item}
+        alt=""
         style={{
           width: "100%",
-          height: "170px",
+          height: "120px",
           objectFit: "contain",
-          marginBottom: "10px"
+          marginBottom: "10px",
         }}
       />
       <p
         style={{
           fontSize: "18px",
           padding: "5px 0",
-          fontWeight: 400,
-          color: "gray"
-        }}
-      >
-        Top Trending Tv
-      </p>
-      <p>
-        <Rating value={Math.random() * 5 + 1} precision={0.5} />
-      </p>
-      <span
-        style={{
-          textDecoration: "line-through",
           color: "gray",
-          marginRight: 7,
-          fontWeight: 300
+          fontWeight: 400,
         }}
       >
-        ${Math.floor(Math.random() * 4300 + 3000)}
-      </span>
-      <span style={{ color: "gray", fontWeight: 700 }}>
-        ${Math.floor(Math.random() * 2300 + 1280)}
-      </span>
-      <p style={{ fontSize: "16px", padding: "5px", color: "green" }}>
-        From 300${" "}
+        Top Trending Tvs
       </p>
-      <p style={{ fontSize: "14px", padding: "5px", color: "gray" }}>
-        Up to 1,000$
+      <Rating precision={0.5} size="small" />
+      <p>
+        <span
+          style={{
+            textDecoration: "line-through",
+            color: "gray",
+            marginRight: 7,
+            fontWeight: 300,
+          }}
+        >
+          $7170
+        </span>
+        <span style={{ color: "gray", fontWeight: 700 }}>$7071</span>
       </p>
-      <button>Add to card</button>
+      <button>Add to cart</button>
     </div>
   );
 };
 
-export default BootstrapMultiCarouse;
+export default BootstrapMultiCarousel;
